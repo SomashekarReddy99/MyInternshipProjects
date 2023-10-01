f527c9a9ae6b811f7b33a60d3a1be39a28da0abd
SomashekarReddy99 <146644027+SomashekarReddy99@users.noreply.github.com>
2023-10-01 20:41:24 +0530

    Add files via upload
    my portofolio website

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function menu(){
+   const menu=document.querySelector(".menu_links");
+   const icon=document.querySelector(".icon");
+   menu.classList.toggle("open");
+   icon.classList.toggle("open");
+}
+var typed = new Typed(".p2", {
+    strings: ["Student", "Developer", "Technophile", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".p4", {
+    strings: ["Student", "Developer", "Technophile", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
